e708c8e5b98a2ed01b2df65c11dbc72d811568ab
John Bullock <john@johnbullock.org>
2020-10-10 11:42:38 -0500

    Add JS to shrink margin-bottom btw. <p> and <pre>
    Helps for display of code blocks after <p>

--- Bullock_xaringan.js
+++ Bullock_xaringan.js
@@ -1,5 +1,13 @@
 <script>
+  // The <script> tags are necessary when being source by xaringan.
+  // [2020 10 10].
+  
+  
   // window.onload = function () {
+
+    /* Reduce gap between a paragraph and the following code block. */
+    myPre = document.querySelectorAll("p + pre");
+    for (const pre of myPre) { pre.previousElementSibling.style.marginBottom = "0.2em" };
 
     /* Adjust list numbering. It may be possible to do this more 
        elegantly with a remark.js macro, such that I don't need a 
@@ -11,5 +19,5 @@
     let myLists4 = document.querySelectorAll(".listStart4 > ol");
     myLists4.forEach(function (x) { x.setAttribute("start", 4) } );
 
-}
+//}
 </script>
