d2158e244bc88571b95aad288e99f94d2f8aa8c3
John Bullock <john@johnbullock.org>
2020-10-13 10:32:56 -0500

    Improve functions for changing list start numbers
    Now, the functions find any <ol> tag in a given slide -- not just <ol> tags that are children of the slide-parent <div> tags.

--- Bullock_xaringan.js
+++ Bullock_xaringan.js
@@ -13,10 +13,10 @@
        elegantly with a remark.js macro, such that I don't need a 
        different pair of JS commands for each new start number. See 
        https://slides.yihui.org/xaringan/#33. */
-    let myLists3 = document.querySelectorAll(".listStart3 > ol");
+    let myLists3 = document.querySelectorAll(".listStart3 ol");
     myLists3.forEach(function (x) { x.setAttribute("start", 3) } );
     
-    let myLists4 = document.querySelectorAll(".listStart4 > ol");
+    let myLists4 = document.querySelectorAll(".listStart4 ol");
     myLists4.forEach(function (x) { x.setAttribute("start", 4) } );
 
 //}
