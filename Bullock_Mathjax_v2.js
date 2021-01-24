9b8a33b579886337c64758698e0e5c51d8ca35cc
John Bullock <john@johnbullock.org>
2021-01-24 12:33:16 -0600

    Add \matwo macro.

--- Bullock_Mathjax_v2.js
+++ Bullock_Mathjax_v2.js
@@ -6,6 +6,8 @@
   // To insert this script into an R Markdown file, put it in a 
   // <script type="text/x-mathjax-config"> container.  [2020 02 17]
 
+  // On defining TeX macros, see http://docs.mathjax.org/en/latest/input/tex/macros.html.
+
   MathJax.Hub.Config({ 
     AssistiveMML: { disabled: true },  
     jax: ["input/TeX", "output/CommonHTML"],
@@ -20,6 +22,7 @@
         cov: ["\\textrm{cov}\\backtwo\\left(#1\\right)", 1],
         E: ["E\\backtwo\\left[#1\\right]", 1],
         independent: "\\perp\\!\\!\\!\\perp",
+        matwo: ["\\ensuremath{\\left[\\begin{array}{#1} #2 & #3\\\\#4 & #5\\end{array}\\right]}", 5],
         sd: ["\\textrm{sd}\\backtwo\\left(#1\\right)", 1],
         var: ["\\textrm{var}\\backtwo\\left(#1\\right)", 1]
 
