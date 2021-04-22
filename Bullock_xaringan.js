25fcb67d3c9c3316cbcd51f984dd491bd2e9327d
John Bullock <john@johnbullock.org>
2021-04-22 16:32:21 -0500

    Add JS for .listStart6, .listStart7.

--- Bullock_xaringan.js
+++ Bullock_xaringan.js
@@ -25,7 +25,9 @@
   /* Adjust list numbering. It may be possible to do this more 
      elegantly with a remark.js macro, such that I don't need a 
      different pair of JS commands for each new start number. See 
-     https://slides.yihui.org/xaringan/#33. */
+     https://slides.yihui.org/xaringan/#33. See also the "startnum" pandoc 
+     Markdown extension described at https://pandoc.org/MANUAL.html#pandocs-markdown; 
+     it may be that I don't need JS for this at all. */
   let myLists3 = document.querySelectorAll(".listStart3 ol");
   myLists3.forEach(function (x) { x.setAttribute("start", 3) } );
   
@@ -34,5 +36,12 @@
 
   let myLists5 = document.querySelectorAll(".listStart5 ol");
   myLists5.forEach(function (x) { x.setAttribute("start", 5) } );
+  
+  let myLists6 = document.querySelectorAll(".listStart6 ol");
+  myLists6.forEach(function (x) { x.setAttribute("start", 6) } );
+
+  let myLists7 = document.querySelectorAll(".listStart7 ol");
+  myLists7.forEach(function (x) { x.setAttribute("start", 7) } );
+  
 //}
 </script>
